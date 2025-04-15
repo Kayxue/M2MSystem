1f94cb7eb7d83b379717a69b5c00007147853e61
Kayxue <s0990006@gmail.com>
2025-04-15 12:56:47 +0800

    Finished get home application route

---
+++ src/routes/Application.rs
@@ -0,0 +1,39 @@
+use sea_orm::{ColumnTrait, EntityTrait, QueryFilter, Related};
+use serde::Deserialize;
+use xitca_web::error::Error;
+use xitca_web::{
+    codegen::route,
+    handler::{json::Json, params::LazyParams, state::StateRef},
+};
+
+use crate::AppState;
+use crate::CustomError::*;
+use crate::entities::{prelude::*, *};
+
+use serde::Deserialize;
+
+#[derive(Deserialize)]
+struct GetHomeApplicationParams<'a> {
+    id: &'a str,
+}
+
+#[route("/application/getHomeApplication/:id", method = get)]
+pub async fn getHomeApplication(
+    state: StateRef<'_, AppState>,
+    params: LazyParams<'_, GetHomeApplicationParams<'_>>,
+) -> Result<Json<Vec<application::Model>>, Error> {
+    let GetHomeApplicationParams { id } = params.deserialize()?;
+    if let Ok(home_application) = Home::find()
+        .find_with_related(Application)
+        .filter(home::Column::Id.eq(id.to_owned()))
+        .all(&state.db)
+        .await
+    {
+        if let Some(home) = home_application.first() {
+            return Ok(Json(home.1.clone()));
+        }
+        Err(BadRequest::new("Home not found").into())
+    } else {
+        Err(InternalServerError::new("Query failed").into())
+    }
+}
